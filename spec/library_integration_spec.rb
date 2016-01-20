d50251263dcd7d0309baed04f660dff3a8930946
Krystan Menne and Lauryn Davis <lryndavis@gmail.com>
2016-01-20 15:23:00 -0800

    refactor redundancy in integration test

--- spec/library_integration_spec.rb
+++ spec/library_integration_spec.rb
@@ -53,14 +53,11 @@ describe('delete a book', {:type => :feature}) do
     expect(page).to(have_content('A book has been deleted.'))
     expect(page).not_to(have_content('Jane Eyre'))
   end
-end
-
-describe('patron cannot delete book', {:type => :feature}) do
   it('will not allow a patron to delete a book') do
     create_test_book().save()
     visit('/')
     click_link('Browse All Books')
     expect(page).to(have_content('Jane Eyre'))
     expect(page).not_to(have_content('Remove Book from Database'))
-    end
+  end
 end
